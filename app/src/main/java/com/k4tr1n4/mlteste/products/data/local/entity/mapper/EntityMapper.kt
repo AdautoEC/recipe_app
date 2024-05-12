58bf1aa9624579c2d7f954845a80410285f61c80
Adauto Neto <adauto.neto@bs2tecnologia.com.br>
2024-05-12 15:55:50 -0300

    chore: remove unused database

--- app/src/main/java/com/k4tr1n4/mlteste/products/data/local/entity/mapper/EntityMapper.kt
+++
@@ -1,8 +0,0 @@
-package com.k4tr1n4.mlteste.products.data.local.entity.mapper
-
-interface EntityMapper<Domain, Entity> {
-
-    fun asEntity(domain: Domain): Entity
-
-    fun asDomain(entity: Entity): Domain
-}
